fad4137aa20606ec0c09c84ddf93e1c047976fbb
jbl-inc <jbl@inconsult-online.de>
2023-06-21 17:12:35 +0200

    Added drawing functionality via softbuffer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use winit::{
     event::{Event, WindowEvent},
-    event_loop::{EventLoop, ControlFlow},
+    event_loop::EventLoop,
     window::{WindowBuilder, Window, WindowLevel}, dpi::{PhysicalPosition, LogicalSize},
 };
-
+use std::{collections::HashMap, num::NonZeroU32};
 use tray_icon::{TrayIconBuilder, menu::{Menu, MenuItem, MenuEvent}, TrayEvent};
 
 enum MenuAction {
     Exit,
     None
+}
+
+const WORK_SECONDS: i64 = 10;
+const BREAK_SECONDS: i64 = 3;
+
+const RED: u32 = 0xFFFF0000;
+const GREEN: u32 = 0xFF00FF00;
+
+enum WorkState {
+    Working,
+    Break
 }
 
 fn main() {
@@ -24,12 +35,10 @@ fn main() {
     tray_menu.append(&item2);
     tray_menu.append(&exit_item);
 
-    //A dictionary of Strings and the corresponding MenuItem ids
-    let menu_dict: Vec<(MenuAction, u32)> = vec![
-        (MenuAction::None, item1.id()), 
-        (MenuAction::None, item2.id()), 
-        (MenuAction::Exit, exit_item.id())
-    ];
+    let mut menu_dict: HashMap<u32, MenuAction> = HashMap::new();
+    menu_dict.insert(item1.id(), MenuAction::None);
+    menu_dict.insert(item2.id(), MenuAction::None);
+    menu_dict.insert(exit_item.id(), MenuAction::Exit);
 
     let _tray_icon = TrayIconBuilder::new()
         .with_menu(Box::new(tray_menu))
@@ -40,9 +49,9 @@ fn main() {
 
     let event_loop = EventLoop::new();
     
-    let _window: Window = WindowBuilder::new()
+    let window: Window = WindowBuilder::new()
         .with_decorations(false)
-        .with_transparent(true)
+        .with_transparent(false)
         .with_inner_size(LogicalSize::new(50, 50))
         .with_position(PhysicalPosition::new(50, 920))
         .with_window_level(WindowLevel::AlwaysOnTop)
@@ -50,17 +59,40 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
-
+    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
+    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
+    
     let menu_channel = MenuEvent::receiver();
     let tray_channel = TrayEvent::receiver();
 
-    let mut i = 0;
+    let mut start_time = chrono::offset::Local::now();
+
+    let mut work_state = WorkState::Working;
 
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
+        //*control_flow = ControlFlow::Poll;
 
-        //println!("{}", i);
-        //i += 1;
+        let c_stamp = chrono::offset::Local::now().timestamp();
+        let s_stamp = start_time.timestamp();
+
+        match work_state {
+            WorkState::Working => {
+                if c_stamp - s_stamp > WORK_SECONDS {
+                    work_state = WorkState::Break;
+                    start_time = chrono::offset::Local::now();
+                    window.request_redraw();
+                    println!("Changed to Break State");
+                }
+            }
+            WorkState::Break => {
+                if c_stamp - s_stamp > BREAK_SECONDS {
+                    work_state = WorkState::Working;
+                    start_time = chrono::offset::Local::now();
+                    window.request_redraw();
+                    println!("Changed to Work State");
+                }
+            }
+        }
 
         if let Ok(event) = tray_channel.try_recv() {
             println!("{event:?}");
@@ -71,6 +103,27 @@ fn main() {
         }
 
         match event {
+            Event::RedrawRequested(_window_id) => {
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
+                    let mut buffer = surface.buffer_mut().unwrap();
+                    for index in 0..((width * height) as usize) {
+                        buffer[index] = match work_state {
+                            WorkState::Working => RED,
+                            WorkState::Break => GREEN
+                        };
+                    }
+
+                    buffer.present().unwrap();
+            }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
